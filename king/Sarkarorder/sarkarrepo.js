7837b064262f361f870823c6479fb1cee6586804
Sarkar~Tech <bandahealimaree@gmail.com>
2024-11-21 13:33:27 +0500

    Update sarkarrepo.js

--- king/Sarkarorder/sarkarrepo.js
+++ king/Sarkarorder/sarkarrepo.js
@@ -10,7 +10,7 @@ const handleRepoCommand = async (m, Matrix) => {
 
     const { full_name, name, forks_count, stargazers_count, created_at, updated_at } = repoData;
 
-    const messageText = `Hello *_${m.pushName}_,*\nThis is *Ethix-MD,* a WhatsApp bot built by *Ethix-Xsid,* enhanced with amazing features to make your WhatsApp communication and interaction experience amazing!\n\n*❲❒❳ Name:* ${name}\n*❲❒❳ Stars:* ${stargazers_count}\n*❲❒❳ Forks:* ${forks_count}\n*❲❒❳ Created On:* ${new Date(created_at).toLocaleDateString()}\n*❲❒❳ Last Updated:* ${new Date(updated_at).toLocaleDateString()}\n\n*© Powered by Ethix-MD*`;
+    const messageText = `Hello *_${m.pushName}_,*\nThis is *Sarkar-MD,* a WhatsApp bot Created by *Bandaheali,* enhanced with amazing features to make your WhatsApp communication and interaction experience amazing!\n\n*❲❒❳ Name:* ${name}\n*❲❒❳ Stars:* ${stargazers_count}\n*❲❒❳ Forks:* ${forks_count}\n*❲❒❳ Created On:* ${new Date(created_at).toLocaleDateString()}\n*❲❒❳ Last Updated:* ${new Date(updated_at).toLocaleDateString()}\n\n*© Powered by Ethix-MD*`;
 
     await Matrix.sendMessage(m.from, { text: messageText }, { quoted: m });
     await m.React("✅");
