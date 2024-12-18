68fe37660f6f4a9307e67d6205a18818e85ccb0d
Sarkar~Tech <bandahealimaree@gmail.com>
2024-12-18 16:13:26 +0500

    Update index.js

--- king/Sarkar/index.js
+++ king/Sarkar/index.js
@@ -1,4 +1,5 @@
 import Handler from './handler.js'
 import Callupdate from './call-handler.js'
 import GroupUpdate from './group-handler.js'
-export { Handler, Callupdate, GroupUpdate };
+import chatbotCommand from './chatbot.js'
+export { Handler, Callupdate, chatbotCommand, GroupUpdate };
