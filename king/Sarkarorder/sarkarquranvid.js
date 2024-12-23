6aead7c44ed83a29e34de740c0b229f5d96c129c
Sarkar~Tech <bandahealimaree@gmail.com>
2024-12-23 20:09:12 +0500

    Update sarkarquranvid.js

--- king/Sarkarorder/sarkarquranvid.js
+++ king/Sarkarorder/sarkarquranvid.js
@@ -5,10 +5,10 @@ const quranVideo = async (m, gss) => {
   const prefix = config.PREFIX;
   const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
   const validCommands = ['quranvid', 'qvid', 'quranvideo'];
-await m.React('⏳'); // React with a loading icon
+
   if (validCommands.includes(cmd)) {
     const videoUrl = `https://bk9.fun/Islam/quranvid`;
-
+await m.React('⏳'); // React with a loading icon
     await gss.sendMessage(
       m.from,
       {
